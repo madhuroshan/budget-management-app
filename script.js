aab70c3e526a18553ecaca6a6967a1f745f091e0
madhuroshan <madhuroshan2102@gmail.com>
2022-12-24 16:08:19 +0530

    created file system

---
+++ script.js
@@ -0,0 +1,21 @@
+class Movement {
+  constructor() {}
+}
+
+class App {
+  constructor() {}
+
+  _showForm() {}
+
+  _hideForm() {}
+
+  _updateCurrentMovement() {}
+
+  _updateBalance() {}
+
+  _getLocalStorage() {}
+
+  _setLocalStorage() {}
+}
+
+const app = new App();
